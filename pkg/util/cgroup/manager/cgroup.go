2921e4b4782ddd998b4d3f08e3e8984a06b70b73
zhangrenpeng <zhangrenpeng@bytedance.com>
2026-03-16 14:48:12 +0800

    refactor(cgroup): Replace the hard-coded dying memcg reclamation threshold with a constant.

--- pkg/util/cgroup/manager/cgroup.go
+++ pkg/util/cgroup/manager/cgroup.go
@@ -50,6 +50,8 @@ const (
 
 const CgroupFSMountPoint = "/sys/fs/cgroup"
 
+const DyingMemcgThreshold int = 2000
+
 func ApplyMemoryWithRelativePath(relCgroupPath string, data *common.MemoryData) error {
 	if data == nil {
 		return fmt.Errorf("ApplyMemoryWithRelativePath with nil cgroup data")
@@ -613,12 +615,12 @@ func DyingMemcgReclaimWithAbsolutePath(ctx context.Context, absCGPath string, em
 	nrDyingDescendants := initialDyingDescendants
 
 	for i := 0; i < 10; i++ {
-		if nrDyingDescendants <= 2000 {
-			general.Infof("nr_dying_descendants: %d < 2000, no need to reclaim", nrDyingDescendants)
+		if nrDyingDescendants <= DyingMemcgThreshold {
+			general.Infof("nr_dying_descendants: %d <= %d, no need to reclaim", nrDyingDescendants, DyingMemcgThreshold)
 			break
 		}
 
-		general.Infof("nr_dying_descendants: %d > 2000, reclaim memory", nrDyingDescendants)
+		general.Infof("nr_dying_descendants: %d > %d, reclaim memory", nrDyingDescendants, DyingMemcgThreshold)
 
 		// invoke memory reclaim
 		err = invokeMemoryReclaim(reclaimFile, "30m")
